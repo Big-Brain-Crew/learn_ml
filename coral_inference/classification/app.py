180d28a7b1a3a5c781b45aa5a8c2432f24cf7846
Swapnil Pande <swapnil.pande98@gmail.com>
2020-05-14 16:34:08 -0400

    Added infrastructure for coral inference app to publish classification results on a webpage

---
+++ coral_inference/classification/app.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+from flask import Flask, render_template, Response
+import cv2
+from classifier import Classifier
+
+
+# Instantiate a Flask app
+app = Flask(__name__)
+
+
+# Define index pag
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+# Define generator function that yields frames from the classifier
+# The classifier is responsible for capturing a frame from the camera, processing it, and returning the results
+def gen(camera):
+    while True:
+        frame = camera.get_frame()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')
+
+# Define the video feed on the webpage
+@app.route('/video_feed')
+def video_feed():
+    return Response(gen(Camera()),
+                    mimetype='multipart/x-mixed-replace; boundary=frame')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', debug=True)
