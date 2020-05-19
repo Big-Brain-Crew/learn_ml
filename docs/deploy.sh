0744ffe97dea9523126cf6a240a268164ec27e68
Swapnil Pande <swapnil.pande98@gmail.com>
2020-05-19 14:31:27 -0400

    Testing bugs with deploy

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -2,19 +2,21 @@
 
 # cd out of the repo
 cd ..
+ls
 
 # Clone the repo to the gh-pages branch
 git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
 
 cd gh-pages
+ls
 
 # Update git config.
 git config user.name "Travis Builder"
 git config user.email "$GH_EMAIL"
 
 # Copy the HTML files to the gh-pages branch
-cp -R ../$REPO/docs/_build/html/* ./
+#cp -R ../$REPO/docs/_build/html/* ./
 
-git add -add
-git commit "Autodoc commit for $COMMIT."
-git push -q origin gh-pages
+#git add -A
+#git commit "Autodoc commit for $COMMIT."
+#git push -q origin gh-pages
