2cddbb1ed88950fbff73182ce746575b8bdb7e6c
Swapnil Pande <swapnil.pande98@gmail.com>
2020-05-19 14:50:07 -0400

    WIP #7: Added pushing gh-pages to build script

--- docs/deploy.sh
+++ docs/deploy.sh
@@ -6,7 +6,7 @@ REPO=learn_ml
 
 # cd out of the repo
 cd ..
-ls
+
 
 # Clone the repo to the gh-pages branch
 git clone -b gh-pages "https://$GH_TOKEN@github.com/$ORG/$REPO.git" gh-pages
@@ -19,8 +19,8 @@ git config user.name "Travis Builder"
 git config user.email "$GH_EMAIL"
 
 # Copy the HTML files to the gh-pages branch
-#cp -R ../$REPO/docs/_build/html/* ./
+cp -R ../$REPO/docs/_build/html/* ./
 
-#git add -A
-#git commit "Autodoc commit for $COMMIT."
-#git push -q origin gh-pages
+git add -A
+git commit "Autodoc commit for $COMMIT."
+git push -q origin gh-pages
