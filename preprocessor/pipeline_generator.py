893619e09d6d44c990339e4bf3ece5b2b6072092
Alex Barnett <alexander.barnett@vanderbilt.edu>
2020-05-20 11:49:31 -0500

    Created private methods

--- preprocessor/pipeline_generator.py
+++ preprocessor/pipeline_generator.py
@@ -30,102 +30,102 @@ class PipelineGenerator(object):
         self.indent_level = 0  # Keeps track of current indentation
         self.indent_str = "    "
 
-    def indent(self, inc=1):
+    def __indent(self, inc=1):
         ''' Increments the indent level of the output string.
         '''
 
         self.indent_level += inc
 
-    def unindent(self, dec=1):
+    def __unindent(self, dec=1):
         ''' Decrements the indent level of the output string.
         '''
 
         self.indent_level -= dec
 
-    def spaces(self):
+    def __spaces(self):
         '''Returns a string of the correct number of spaces for the current indent level.
         '''
 
         return self.indent_str * self.indent_level
 
-    def write(self, line):
+    def __write(self, line):
         ''' Writes a line of code to the file.
         '''
 
-        self.out.write(self.spaces() + line)
+        self.out.write(self.__spaces() + line)
 
-    def write_docstring(self, line):
+    def __write_docstring(self, line):
         ''' Writes a line of docstring to the file.
             line: Docstring comment without docstring quotes.
         '''
 
-        self.out.write(self.spaces() + "'''" + line)
-        self.out.write(self.spaces() + "'''\n\n")
+        self.out.write(self.__spaces() + "'''" + line)
+        self.out.write(self.__spaces() + "'''\n\n")
 
-    def start_method(self):
+    def __start_method(self):
         '''Formats out string to generate a method.
         '''
 
-        self.indent()
+        self.__indent()
 
-    def end_method(self):
+    def __end_method(self):
         '''Formats out string once method is done.
         '''
 
-        self.write("\n\n")
-        self.unindent(2)
+        self.__write("\n\n")
+        self.__unindent(2)
 
-    def imports(self):
+    def __imports(self):
         ''' Generate import statements.
         '''
 
-        self.write("# Imports\n")
-        self.write("import os\n")
-        self.write("import sys\n")
-        self.write("sys.path.append(os.path.join(os.getcwd(), \"preprocessor\"))\n")
-        self.write("import tensorflow as tf\n")
-        self.write("import tensorflow_datasets as tfds\n")
-        self.write("import json\n")
-        self.write("from tf_utils import *\n")
-        self.write("\n\n")
+        self.__write("# Imports\n")
+        self.__write("import os\n")
+        self.__write("import sys\n")
+        self.__write("sys.path.append(os.path.join(os.getcwd(), \"preprocessor\"))\n")
+        self.__write("import tensorflow as tf\n")
+        self.__write("import tensorflow_datasets as tfds\n")
+        self.__write("import json\n")
+        self.__write("from tf_utils import *\n")
+        self.__write("\n\n")
 
-    def class_def(self):
+    def __class_def(self):
         ''' Generate DatasetPipeline class definition.
         '''
 
         # DatasetPipeline class
-        self.write("class DatasetPipeline(object):\n")
-        self.indent()
-        self.write_docstring("Represents a dataset that has been preprocessed.\n")
+        self.__write("class DatasetPipeline(object):\n")
+        self.__indent()
+        self.__write_docstring("Represents a dataset that has been preprocessed.\n")
 
         # Init
-        self.write("def __init__(self):\n\n")
-        self.indent()
-        self.write(
+        self.__write("def __init__(self):\n\n")
+        self.__indent()
+        self.__write(
             "(self.ds_train, self.ds_test), self.ds_info = self.load_dataset()\n\n")
-        self.write("self.preprocess()\n")
+        self.__write("self.preprocess()\n")
 
-        self.end_method()
+        self.__end_method()
 
-    def load_dataset(self):
+    def __load_dataset(self):
         '''Generate code for loading the dataset.
         '''
 
-        self.start_method()
-        self.write("def load_dataset(self):\n")
-        self.indent()
-        self.write_docstring("Load the dataset from Tensorflow Datasets.\n")
+        self.__start_method()
+        self.__write("def load_dataset(self):\n")
+        self.__indent()
+        self.__write_docstring("Load the dataset from Tensorflow Datasets.\n")
 
         # Load dataset from Tensorflow Datasets
-        self.write("return tfds.load(\'{ds}\',".format(ds=self.dataset) +
-                   "split=['train', 'test']," +
-                   "shuffle_files=True," +
-                   "as_supervised=True," +
-                   "with_info = True)\n")
+        self.__write("return tfds.load(\'{ds}\',".format(ds=self.dataset) +
+                     "split=['train', 'test']," +
+                     "shuffle_files=True," +
+                     "as_supervised=True," +
+                     "with_info = True)\n")
 
-        self.end_method()
+        self.__end_method()
 
-    def map_variables(self, params):
+    def __map_variables(self, params):
         ''' Convert parameter value representations to their actual values.
             This is necessary to keep the pipeline config file independent of
             specific function names, i.e. tf2 function calls.
@@ -141,19 +141,19 @@ class PipelineGenerator(object):
 
         return params
 
-    def operations(self):
+    def __operations(self):
         ''' Generate code for all preprocessing operations.
         '''
 
-        self.start_method()
-        self.write("def preprocess(self):\n")
-        self.indent()
-        self.write_docstring("Apply data preprocessing operations.\n")
+        self.__start_method()
+        self.__write("def preprocess(self):\n")
+        self.__indent()
+        self.__write_docstring("Apply data preprocessing operations.\n")
 
         for _op in self.ops:
 
             # Retrieve variables and map their values
-            variables = self.map_variables(list(_op.values())[0])
+            variables = self.__map_variables(list(_op.values())[0])
 
             # Define the Tensorflow function
             fn_str = 'self.ds_train = self.ds_train.{fn}'.format(fn=list(_op)[0])
@@ -165,41 +165,49 @@ class PipelineGenerator(object):
 
             # Concatenate them all
             op_str = fn_str + '(' + param_str + ')\n'
-            self.write(op_str)
+            self.__write(op_str)
 
-        self.end_method()
+        self.__end_method()
 
-    def helper_funcs(self):
+    def __helper_funcs(self):
         ''' Generates get/set helper functions.
         '''
 
         # def get_training_dataset()
-        self.start_method()
-        self.write("def get_training_dataset(self):\n")
-        self.indent()
-        self.write_docstring("Returns the training dataset.\n")
-        self.write("return self.ds_train\n")
+        self.__start_method()
+        self.__write("def get_training_dataset(self):\n")
+        self.__indent()
+        self.__write_docstring("Returns the training dataset.\n")
+        self.__write("return self.ds_train\n")
 
-        self.end_method()
+        self.__end_method()
 
         # def get_test_dataset()
-        self.start_method()
-        self.write("def get_test_dataset(self):\n")
-        self.indent()
-        self.write_docstring("Returns the test dataset.\n")
-        self.write("return self.ds_test\n")
+        self.__start_method()
+        self.__write("def get_test_dataset(self):\n")
+        self.__indent()
+        self.__write_docstring("Returns the test dataset.\n")
+        self.__write("return self.ds_test\n")
 
-        self.end_method()
+        self.__end_method()
 
     def gen_pipeline(self):
         ''' Generate all code part of the dataset pipeline.
         '''
 
-        self.imports()
-        self.class_def()
-        self.load_dataset()
-        self.operations()
-        self.helper_funcs()
+        self.__imports()
+        self.__class_def()
+        self.__load_dataset()
+        self.__operations()
+        self.__helper_funcs()
+
+    def get_pipeline_file_name(self):
+        '''Returns the name of the generated pipeline script.
+
+        Currently the file name is not modifiable.
+        '''
+
+        return self.out
 
 
 def main():
