7a8064695073e3d106633fa0554055cb7c0c8345
Alex Barnett <alexander.barnett@vanderbilt.edu>
2020-05-26 18:13:13 -0500

    idk, I ran pylint, maybe this fixes it the docs build?

--- generators/generator_utils.py
+++ generators/generator_utils.py
@@ -1,66 +1,65 @@
+''' Utility functions for code generators.
+'''
+
 import numpy as np
-import pdb
+
 
 def _is_valid_arg_dict(arg_dict):
-        ''' Check if arguments are formatted correctly in the dictionary.
+    ''' Check if arguments are formatted correctly in the dictionary.
 
-        The argument dict must follow the convention of Python function arguments. Args with
-        default parameters must come after args without. All keys must be of type string. 
-        Values may take the forms:
-             string: To represent any object. Will be written without quotes.
-             int/float: To represent a number value.
-             list: A list input.
-             dict: To represent a function as a value.
-             None: This can be used to signify there is no default parameter. 
+    The argument dict must follow the convention of Python function arguments. Args with
+    default parameters must come after args without. All keys must be of type string.
+    Values may take the forms:
+         string: To represent any object. Will be written without quotes.
+         int/float: To represent a number value.
+         list: A list input.
+         dict: To represent a function as a value.
+         None: This can be used to signify there is no default parameter.
 
-        This function raises exceptions if any of these rules are not followed.
+    This function raises exceptions if any of these rules are not followed.
 
-        Args:
-            arg_dict: A dict of method arguments in the form ("param" : "value"). param must be 
-            string and value may be string or None. arg_dict can be None instead of dict
-            if there are no args
+    Args:
+        arg_dict: A dict of method arguments in the form ("param" : "value"). param must be
+        string and value may be string or None. arg_dict can be None instead of dict
+        if there are no args
 
-        Raises:
-            TypeError: Arguments must be formatted as a dictionary.
-            ValueError: All None values must come before all string values.
-            TypeError: All keys must be strings and all values must be strings or None.
-        '''
-        # Args must be in a dict
-        if arg_dict:
-            if not isinstance(arg_dict, dict):
-                raise TypeError("Arguments must be formatted as a dictionary.")
+    Raises:
+        TypeError: Arguments must be formatted as a dictionary.
+        ValueError: All None values must come before all string values.
+        TypeError: All keys must be strings and all values must be strings or None.
+    '''
+    # Args must be in a dict
+    if arg_dict:
+        if not isinstance(arg_dict, dict):
+            raise TypeError("Arguments must be formatted as a dictionary.")
 
-            def allowed_val_type(v):
-                return (isinstance(v, str) or
-                        isinstance(v, int) or
-                        isinstance(v, float) or
-                        isinstance(v, list) or
-                        isinstance(v, dict) or
-                        v is None)
+        def allowed_val_type(val):
+            return (isinstance(val, (dict, float, int, list, str)) or val is None)
 
-            # Check that all keys are strings and all vals are string, int or None
-            all_str_keys = [isinstance(k, str) for k in arg_dict.keys()]
-            all_str_vals = [allowed_val_type(v) for v in arg_dict.values()]
+        # Check that all keys are strings and all vals are string, int or None
+        all_str_keys = [isinstance(k, str) for k in arg_dict.keys()]
+        all_str_vals = [allowed_val_type(v) for v in arg_dict.values()]
 
-            # Check for default parameters before args w/o default parameters
-            bad_arg_order = False
-            default_param = False
-            for val in arg_dict.values():
-                if val and not default_param:
-                    default_param = True
-                elif not val and default_param:
-                    bad_arg_order = True
+        # Check for default parameters before args w/o default parameters
+        bad_arg_order = False
+        default_param = False
+        for val in arg_dict.values():
+            if val and not default_param:
+                default_param = True
+            elif not val and default_param:
+                bad_arg_order = True
 
-            if bad_arg_order:
-                raise ValueError("All None values must come before all string values.")
-            if not np.all(all_str_keys) or not np.all(all_str_vals):
-                raise TypeError("All keys must be strings and all values must be strings, \
+        if bad_arg_order:
+            raise ValueError("All None values must come before all string values.")
+        if not np.all(all_str_keys) or not np.all(all_str_vals):
+            raise TypeError("All keys must be strings and all values must be strings, \
                     int, float, or None.")
+
 
 def _is_valid_fn_dict(fn_dict):
     ''' Check if function is formatted correctly in a dictionary.
 
-    The function dictionary must be correctly formatted so it can be parsed. It must have two 
+    The function dictionary must be correctly formatted so it can be parsed. It must have two
     keys: "name" and "args". The value of "name" must be a string, and args must be formatted
     as an arg_dict (see _is_valid_arg_dict for that format). Any input deviating from this will
     raise an exception.
@@ -73,7 +72,7 @@ def _is_valid_fn_dict(fn_dict):
         ValueError: Must have a "name" key in the dict.
         ValueError: Must have an "args" key in the dict.
         ValueError: The only keys must be "name' and "args".
-        TypeError: Function name must be a string. 
+        TypeError: Function name must be a string.
     '''
     if not isinstance(fn_dict, dict):
         raise TypeError("fn_dict must be of type dict.")
@@ -92,6 +91,7 @@ def _is_valid_fn_dict(fn_dict):
         raise TypeError("Function name must be a string.")
 
     _is_valid_arg_dict(fn_dict["args"])
+
 
 def create_fn_dict(name, args=None):
     ''' Creates a dictionary representation of a function.
