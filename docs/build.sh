edb6b3a18fe8490b3aee710004a58356cc3b41e6
Swapnil Pande <swapnil.pande98@gmail.com>
2020-05-26 11:43:54 -0400

    Determining output on stderr

--- docs/build.sh
+++ docs/build.sh
@@ -5,12 +5,16 @@ cd docs
 
 # Build API docs from the docstrings
 # || { echo 'sphinx-apidoc failed' ; exit 1; }
-before=$(stat -L -c %y /proc/self/fd/2)
-if sphinx-apidoc -q --implicit-namespaces -f -o . .. ../docs/* &&
- after=$(stat -L -c %y /proc/self/fd/2) &&
- [ "$after" = "$before" ]
-then echo 'command ok'
-else echo 'command fail'
+# before=$(stat -L -c %y /proc/self/fd/2)
+# if sphinx-apidoc -q --implicit-namespaces -f -o . .. ../docs/* &&
+#  after=$(stat -L -c %y /proc/self/fd/2) &&
+#  [ "$after" = "$before" ]
+# then echo 'command ok'
+# else echo 'command fail'
+# fi
+
+if { phinx-apidoc -q --implicit-namespaces -f -o 2>&1 >&3 3>&- | grep '^' >&2; } 3>&1; then
+  echo there was some output on stderr
 fi
 
 
