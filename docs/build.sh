5768a983861eaadf3d06cbbb6d6389ad17309317
Swapnil Pande <swapnil.pande98@gmail.com>
2020-05-26 16:15:26 -0400

    Configured makefile to throw errors

--- docs/build.sh
+++ docs/build.sh
@@ -13,6 +13,17 @@ then echo 'Docs built successfully!'
 else exit 1
 fi
 
+# before=$(stat -L -c %y /proc/self/fd/2)
+# if sphinx-apidoc --implicit-namespaces -f -o . .. ../docs/* &&
+#  after=$(stat -L -c %y /proc/self/fd/2) &&
+#  [ "$after" = "$before" ]
+# then echo 'Docs built successfully!'
+# else exit 1
+# fi
+
+sphinx-apidoc --implicit-namespaces -f -o . .. ../docs/*
+
+
 # echo hello
 # if { sphinx-apidoc -q --implicit-namespaces -f -o . .. ../docs/* 2>&1 >&3 3>&- | grep '^' >&2; } 3>&1; then
 #   exit 1
